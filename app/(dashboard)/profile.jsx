ad531e6f44b52203ff7cbdaf4b0071669488080f
Tushar Kumar <tkgoyal104@gmail.com>
2025-12-23 19:05:58 +0530

    Authentication Updated

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -2,17 +2,28 @@ import { StyleSheet, Text, View } from 'react-native'
 import ThemedView from '../../components/ThemedView'
 import ThemeText from '../../components/ThemeText'
 import Spacer from '../../components/Spacer'
+import { useUser } from '../../hooks/useUser'
+import ThemedButton from '../../components/ThemedButton'
 
 const Profile = () => {
+
+    const { logout, user } = useUser()
+
   return (
     <ThemedView style={styles.container} safe={true}>
+        
         <ThemeText title={true} style={styles.heading}>
-            Your Email
+            {user.email}
         </ThemeText>
 
         <Spacer/>
 
         <ThemeText>Time to start reading some books.....</ThemeText>
+
+        <ThemedButton onPress={logout}>
+            <Text style={{color: "#f2f2f2"}}>Logout</Text>
+        </ThemedButton>
+
     </ThemedView>
   )
 }
